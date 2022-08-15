24347d8d62b8c4b9dd275175a739cd73d834bcbe
Angelo Verlain <geoangercola@gmail.com>
2022-08-16 01:41:27 +0200

    use correct pacman

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
+sudo apt install git ninja-build libarchive-dev bsdtar
+
+sudo pip3 install meson
+
+git clone https://gitlab.archlinux.org/pacman/pacman --depth 1
+
+mkdir pacman -p
+cd pacman
+meson build
+cd build
+ninja
+sudo ninja install
+cd ..
 
 source build-pkgbuild.sh
 
