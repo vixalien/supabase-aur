57dd727232c965853a5502d3ac032e60b38ab77d
Angelo Verlain <geoangercola@gmail.com>
2022-08-16 01:23:52 +0200

    add script to install arch linux

--- build.sh
+++ build.sh
@@ -1,13 +1,3 @@
-sudo aptitude install libarchive-dev bsdtar
-
-git clone git://projects.archlinux.org/pacman.git
-
-cd pacman
-./autogen.sh
-./configure --disable-doc
-make
-sudo make install
-cd ..
 
 source build-pkgbuild.sh
 
